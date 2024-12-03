d655907622cf16f5266b157ad50f012c8a25d3d7
Comfort Ngwenya <>
2024-12-03 09:41:23 +0200

    Adding the colors file

---
+++ app/constants/colors.js
@@ -0,0 +1,34 @@
+const colors = {
+    primary: {
+      main: '#18D9A2',
+      light: '#00be00',
+      dark: '#005a00',
+    },
+    secondary: {
+      main: '#ffd700',
+      light: '#ffff40',
+      dark: '#c7a500',
+    },
+    background: {
+      main: '#1A1C33',
+      light: '#ffffff',
+      dark: '#c0c0c0',
+    },
+    text: {
+      primary: '#333333',
+      secondary: '#666666',
+      inverse: '#ffffff',
+    },
+    error: {
+      main: '#ff0000',
+      light: '#ff6666',
+      dark: '#b30000',
+    },
+    success: {
+      main: '#00b300',
+      light: '#66ff66',
+      dark: '#008000',
+    },
+  };
+  
+  export default colors;

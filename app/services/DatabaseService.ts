f6c10ec33b2ec1cc874e68c8877b474f1551f9e8
Comfort Ngwenya <>
2024-12-04 23:17:13 +0200

    Updating the table columns adding the date and time, and adding the delete image method

--- app/services/DatabaseService.ts
+++ app/services/DatabaseService.ts
@@ -11,27 +11,31 @@ class DatabaseService {
 
   private async initDatabase() {
     try {
-      this.db = await SQLite.openDatabaseAsync('gallery.db');
-      await this.db.execAsync(`
-        CREATE TABLE IF NOT EXISTS images (
-          id INTEGER PRIMARY KEY AUTOINCREMENT,
-          uri TEXT NOT NULL,
-          timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
-          latitude REAL,
-          longitude REAL
-        )
-      `);
-      console.log('Database initialized');
-    } catch (error) {
-      console.error('Database initialization error', error);
-    }
-  }
+        this.db = await SQLite.openDatabaseAsync('gallery.db');
+        // Create the table if it does not exist
+        await this.db.execAsync(`
+            CREATE TABLE IF NOT EXISTS images (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                uri TEXT NOT NULL,
+                timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+                latitude REAL,
+                longitude REAL
+            )
+        `);
+        
 
+        console.log('Database initialized');
+    } catch (error) {
+        console.error('Database initialization error', error);
+    }
+}
+
+  
   // Create a new image entry
-  async addImage(uri: string, latitude?: number, longitude?: number) {
+  async addImage(uri: string, latitude?: number, longitude?: number, date?: string, time?: string) {
     return this.db.runAsync(
-      'INSERT INTO images (uri, latitude, longitude) VALUES (?, ?, ?)',
-      [uri, latitude, longitude]
+      'INSERT INTO images (uri, latitude, longitude, date, time) VALUES (?, ?, ?, ?, ?)',
+      [uri, latitude, longitude, date, time]
     );
   }
 
@@ -39,7 +43,10 @@ class DatabaseService {
     return this.db.getAllAsync('SELECT * FROM images ORDER BY timestamp DESC');
   }
 
-
+   // Delete an image entry
+   async deleteImage(id: number) {
+    return this.db.runAsync('DELETE FROM images WHERE id = ?', [id]);
+  }
 
 }
 
